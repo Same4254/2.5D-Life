e24eb7d32f3b0be705c92aa1035bd7da483ac605
Sam <sam.secondo@gmail.com>
2018-01-19 22:33:30 -0500

    Got Rid of old project

--- 2.5D Life - New/src/Entity/FreeMoving/AI/Living/Viewer/DataUpdater.java
+++
@@ -1,6 +0,0 @@
-package Entity.FreeMoving.AI.Living.Viewer;
-
-@FunctionalInterface
-public interface DataUpdater {
-	public abstract float getValue();
-}
--- 2.5D Life - New/src/Input/OnPicked.java
+++
@@ -1,8 +0,0 @@
-package Input;
-
-import com.Engine.PhysicsEngine.Bodies.PhysicsBody;
-
-@FunctionalInterface
-public interface OnPicked {
-	public void pick(PhysicsBody picked);
-}
